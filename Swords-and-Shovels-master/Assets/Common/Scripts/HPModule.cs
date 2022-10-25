3810a686f6a212ca34d2a584b26564c033236ef9
cheoljin0324 <77821929+cheoljin0324@users.noreply.github.com>
2022-10-25 20:10:32 +0900

    AfterSchool HPBarAdd

--- Swords-and-Shovels-master/Assets/Common/Scripts/HPModule.cs
+++ Swords-and-Shovels-master/Assets/Common/Scripts/HPModule.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPModule : MonoBehaviour
 {
+    [SerializeField]
+    Image hpbar;
     // Start is called before the first frame update
+    private int firsthp;
     public int hp = 50;
     MainModule mainModule;
 
     private void Start()
     {
         mainModule = GetComponent<MainModule>();
+        firsthp = hp;
     }
 
 
@@ -19,7 +24,7 @@ public class HPModule : MonoBehaviour
 
         if (mainModule.enabled)
             StartCoroutine(Hitting(_damage));
-
+            hpbar.fillAmount = (float)hp / firsthp;
 
     }
     IEnumerator Hitting(int _damage)
